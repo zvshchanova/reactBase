edf0c4f11a23d8e79dccfb8927ec2de62017c724
CodingCat <mishav147@gmail.com>
2022-11-26 13:33:53 +0100

    Added functions to test

---
+++ baseapp/src/fetchFunction.js
@@ -0,0 +1,12 @@
+
+//Function 5
+//Testing: https://jestjs.io/docs/asynchronous
+export async function fetchData() {
+    const response = await fetch("https://api.github.com/users/pjhyett");
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    return data;
+}
---
+++ baseapp/src/functions.js
@@ -0,0 +1,11 @@
+//Function 1
+export function difference(numA, numB) {
+    return numA - numB;
+}
+
+
+//Function 2
+export function division(numA, numB) {
+    return numA / numB;
+}
+
